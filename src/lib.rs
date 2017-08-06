eec076c0ab39583abb375c5a52b902b8e28f8118
Afshin Mehrabani <afshin.meh@gmail.com>
2017-08-07 00:18:13 +0100

    v0.1.0 :rocket:

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,5 @@
 extern crate assert_approx_eq;
 extern crate rand;
 
-pub mod basic;
 pub mod matrix_trait;
-
-pub struct Matrixnum;
-
-enum Backend {
-    Basic,
-    Ndarray
-}
-
-impl Matrixnum {
-    pub fn new(backend: Backend) -> Box<matrix_trait::MatrixTrait> {
-        return basic::BasicMatrix;
-    }
-}
+pub mod basic;
